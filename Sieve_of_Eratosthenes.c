ab18bf92f7278d8bba831c97b860137258a928ea
nsh <tortoise0929@gmail.com4>
2024-09-18 11:58:03 +0900

    에라토스테네스의 체

---
+++ Sieve_of_Eratosthenes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define max 1000000
+
+int main(){
+	int prime[max]={};
+
+	for (int i = 2; i * i <= max; i++) {
+        if (prime[i] == 0) {
+            for (int j = i * i; j <= max; j += i) {
+                prime[j] = 1;  // 소수가 아님
+            }
+        }
+    }
+
+	int n;
+
+	scanf("%d", &n);
+
+	if(prime[n]==0) printf("True\n");
+	else printf("False\n");
+
+	return 0;
+}
