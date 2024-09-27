ce542524496b3cb82da1b65fa5cd02b1aceed689
namseunghyun <164151319+namseunghyun05@users.noreply.github.com>
2024-09-27 12:43:55 +0900

    Update for efficiency

--- Sieve_of_Eratosthenes.c
+++ Sieve_of_Eratosthenes.c
@@ -5,12 +5,12 @@ int main(){
 	int prime[max]={};
 
 	for (int i = 2; i * i <= max; i++) {
-        if (prime[i] == 0) {
-            for (int j = i * i; j <= max; j += i) {
-                prime[j] = 1;  // 소수가 아님
-            }
-        }
-    }
+        	if (prime[i] == 0) {
+           		 for (int j = i * i; j <= max; j += i) {
+                		prime[j] = 1;  // 소수가 아님
+            		}
+        	}
+    	}
 
 	int n;
 
